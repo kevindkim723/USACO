e4b682da960f5d6f33c3a05e18c23da3d6815e91
Kevin <kevin12kim27@gmail.com>
2019-12-18 22:44:03 -0800

    Namenum
    Permutations work! Times out unfortunately...

--- trainings/namenum.java
+++ trainings/namenum.java
@@ -1,5 +1,100 @@
+/*
+ID: dodocan1
+LANG: JAVA
+TASK: namenum
+*/
+import static java.lang.System.*;
 
-
+import java.util.*;
+import java.util.*;
+import java.io.*;
 public class namenum {
+	static ArrayList<String> corrects = new ArrayList<String>();
+	static String[][] arr = new String[][] { {}, {}, { "A", "B", "C" }, { "D", "E", "F" }, { "G", "H", "I" },
+			{ "J", "K", "L" }, { "M", "N", "O" }, { "P", "R", "S" }, { "T", "U", "V" }, { "W", "X", "Y" } };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader fin = new BufferedReader(new FileReader("namenum.in"));
+		PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter("namenum.out")));
+		
+		String input = fin.readLine();
+		//out.println(input);
+		int[] matrix = new int[input.length()];
+		for (int i = 0; i < input.length();i++)
+		{
+			//out.println(input.charAt(i));
+			matrix[i] = Character.getNumericValue(input.charAt(i));
+		}
+		BufferedReader scan = new BufferedReader(new FileReader("dict.txt"));
+		ArrayList<String> dict = new ArrayList<String>();
+		String line = null;
+		while ((line = scan.readLine()) != null) {
+			dict.add(line);
+		}
+		//out.println(Arrays.toString(matrix));
+		permute(matrix,0,"",dict);
+		//out.println(dict.contains("GREG"));
+		//String[] permutationsArr = permutations.split(" ");
+		//ArrayList<String> correct = new ArrayList<String>();
+		/*for (String s : permutationsArr)
+		{
+			if (dict.contains(s))
+			{
+				correct.add(s);
+			}
+		}*/
+		//out.println(corrects);
+		//Collections.sort(corrects);
+		if (corrects.size()==0)
+		{
+			fout.println("NONE");
+		}
+		else
+		{
+			Collections.sort(corrects);
+		
+			for (String s : corrects)
+			{
+				//out.println(s);
+				fout.println(s);
+			}
+		}
+		fout.close();
+		
+		
+	}
+
+	public static void permute(int[] code, int index,String unfinished, ArrayList<String> dictionary) {
+		//out.println(dictionary);
+		if (index == code.length - 1) // if the index is the last in the code, we want to return that with the
+		// specified permute plus the previous string.
+		{
+			String perm1 = unfinished + arr[code[index]][0];
+			String perm2 = unfinished + arr[code[index]][1];
+			String perm3 = unfinished + arr[code[index]][2];
+			out.println(perm1);
+			out.println(perm2);
+			out.println(perm3);
+			if (dictionary.contains(perm1.trim()))
+			{
+				corrects.add(perm1);
+			}
+			if (dictionary.contains(perm2.trim()))
+			{
+				corrects.add(perm2);
+			}
+			if (dictionary.contains(perm3.trim()))
+			{
+				corrects.add(perm3);
+			}
+
+			return;
+		
+		}
+		for (int i = 0; i < 3; i++) {
+				 permute(code, index + 1, unfinished + arr[code[index]][i],dictionary);
+		}
+		return;
+	}
 
 }
--- trainings/namenumTester.java
+++ trainings/namenumTester.java
@@ -8,23 +8,48 @@ import java.util.*;
 
 public class namenumTester {
 	static ArrayList<String> possibleCombos = new ArrayList<String>();
+	static String[][] arr = new String[][] { {}, {}, { "A", "B", "C" }, { "D", "E", "F" }, { "G", "H", "I" },
+			{ "J", "K", "L" }, { "M", "N", "O" }, { "P", "R", "S" }, { "T", "U", "V" }, { "W", "X", "Y" } };
 
 	public static void main(String[] args) throws FileNotFoundException {
-
-		String[][] arr = new String[][] { {}, {}, { "A", "B", "C" }, { "D", "E", "F" }, { "G", "H", "I" },
-				{ "J", "K", "L" }, { "M", "N", "O" }, { "P", "R", "S" }, { "T", "U", "V" }, { "W", "X", "Y" } };
-
+		
+		int[] arr = { 4, 7, 3, 4 };
 		int num = 4734;
 
-		Scanner scan = new Scanner(new File("/root/dict.txt"));
+		Scanner scan = new Scanner(new File("dict.txt"));
 		ArrayList<String> dict = new ArrayList<String>();
 		while (scan.hasNextLine()) {
 			dict.add(scan.nextLine());
 		}
-		System.out.print(dict);
+		String permutations = permute(arr, 0,"");
+		out.println(permutations);
+		
 	}
 
-	public void mutate(StringBuilder sb, int length, String[][] arr, int index) {
+	public static String permute(int[] code, int index,String unfinished) {
+		String all = "";
+		if (index == code.length - 1) // if the index is the last in the code, we want to return that with the
+		// specified permute plus the previous string.
+		{
+			all += unfinished + arr[code[index]][0] +" ";
+			all += unfinished + arr[code[index]][1] +" ";
+			all += unfinished + arr[code[index]][2] +" ";
+
+			return all;
+			//return unfinished + arr[code[index]][permutenum] +"\n";
+		
+		}
+		for (int i = 0; i < 3; i++) {
+			
+				all += permute(code, index + 1, unfinished + arr[code[index]][i]);
+				
+			
+
+		}
+		return all;
+
+		
+		// if index is not last, we want to continue the permute
 
 	}
 
