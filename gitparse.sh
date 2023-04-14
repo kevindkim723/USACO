81dc7cf7c0b040839bc0031f4b257aab4a07d2ed
Kevin <kevin12kim27@gmail.com>
2023-04-13 23:31:21 -0700

    git parser update

--- gitparse.sh
+++ gitparse.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+function timeRegression {
+    exit
+}
 #checkout the commit, run thing, checkout main.
 log="regression_git.log"
 #holds the line number of distinct commit dates
@@ -7,19 +10,14 @@ index=0
 month="Apr"
 for i in $arr
 do
-    month=$(sed -n ${i}p $log)
-    echo $month
+    commit_date=$(sed -n ${i}p $log)
     ((index++))
     if !((index%5)); then
+        #extract commit id
         commitid=$(sed -n $((i-2))p $log | awk '{print $2}')
-        #commitid=$(awk -v 'n=$((i-2))' '{if(NR==n) print $2}' $log)
-       # commitid=$(awk -v n="$((i-2))" "{if(NR==n) print }" $log | echo '$(awk '{$1=$1};1')' | awk "{print $1}" | awk "{print $1}")
-        #commitid=$(awk -v n="$((i-2))" "{if(NR==n) print }" $log | awk '{print $2}')
         git checkout $commitid
-        sleep 10
+        echo $commit_date >> logger
+        sleep 5
         git checkout master
-        echo $((i-2))
-        echo $commitid
-        echo $index
     fi
 done
